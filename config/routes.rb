a50ce4cad7a69d763614b1564052ff74ff818c90
tomita0731 <175428025+tomita0731@users.noreply.github.com>
2024-10-31 14:36:57 +0000

    [ADD]admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   
+  devise_for :admin, skip: [:registrations, :password], controllers: {
+    sessions: 'admin/sessions'
+  }
+  namespace :admin do
+    get 'dashboards', to: 'dashboards#index'
+    resources :users, only: [:destroy] 
+  end
+  
   devise_for :users
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
